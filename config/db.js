c53dbb02f5ee385dddca9a9c1bada15da67b89dc
Jeffo <geoffrey.nwachuk@gmail.com>
2019-12-10 20:51:13 +0100

    Changed API route

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/knowing',
-    // database: 'mongodb://heroku_17s9tjdt:luppjq1jlmg2ga4c4kfp5s4u9j@ds353338.mlab.com:53338/heroku_17s9tjdt',
+    // database: 'mongodb://localhost:27017/knowing',
+    database: 'mongodb://heroku_17s9tjdt:luppjq1jlmg2ga4c4kfp5s4u9j@ds353338.mlab.com:53338/heroku_17s9tjdt',
     //secret: 'ADD SECRET KEY HERE (OPTIONAL)'
 }
